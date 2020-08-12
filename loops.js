7ccff6bf405369bbc066e039c9664460b54ffaee
Aditya Kale <adityakale3@gmail.com>
2020-08-12 15:48:16 +0530

    Scopre added

--- loops.js
+++ loops.js
@@ -3,4 +3,73 @@
 // for loop // Objects
 // forEach // Arrays
 
+// Data Set
+// var arr = ["Iron Man","Dr. Strange","Captain America","Hulk","SpiderMan", "Deadpool"];
+
+
 // While
+// var a = 0;
+
+// while(a <= arr.length){
+//     console.log("A is at " + a, " | ", arr[a]);
+//     a++;
+// }
+
+
+// For loop
+// for(a = 0; a < 10; a++){
+//     console.log("A is at " + a);
+// }
+
+
+// For Each (Arrays Only)
+// arr.forEach( function(superHero){
+//     // if(superHero.includes('Man')){
+//     //     console.log(superHero);
+//     // }
+//     console.log(superHero);
+// })
+
+// ES6 way of function
+// arr.forEach((superHero) => {
+
+// })
+
+// var arr = ["Iron Man","Dr. Strange","Captain America","Hulk","SpiderMan", "Deadpool"];
+// arr.forEach(function(hero, index){
+//     console.log(hero, index);
+// })
+
+
+// Loop for object
+
+var emp1 = {
+        name : "Aditya",
+        empcode : 20005211,
+        hourlyRate : 10,
+        monthlyHrs : 120,
+
+        salary : function(){
+            var finalSalary = this.monthlyHrs * this.hourlyRate;
+            return finalSalary;
+            //console.log(finalSalary, "/- Rs per Month");
+        }
+}
+
+
+
+for(key in emp1){
+    //console.log(a);
+
+    console.log(emp1[key]);
+}
+
+
+// ES5 way of writting a function
+function displayName(a,b) {
+
+}
+
+// ES6 way of writting a function
+var displayname = (a,b) => { }
+
---
+++ scope.js
@@ -0,0 +1,21 @@
+let king = "Aditya";
+
+// MH
+if(true){
+    //let king = "Iron Man";
+    
+
+    if(true){
+        let king = "Deadpool";
+        console.log("Vid",king);
+    }
+
+}
+
+// Current King
+if(true){
+    console.log("Global King",king);
+}
+
+
+
